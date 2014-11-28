be4845c4705e7398f2c0cc871ec900542daf70b4
Seth Hendrick <xforever1313@aim.com>
2014-11-27 22:29:04 -0500

    Added data and a dummy object, the CE department.

---
+++ js/data.js
@@ -0,0 +1,10 @@
+var mapData = {"mapData": [
+    {"title" : "Computer Engineering Department",
+     "lat" : 43.0845,
+     "lon" : -77.6785,
+     "desc" : "The greatest department ever!",
+     "image" : "ce.png"
+    },
+]
+};
+
