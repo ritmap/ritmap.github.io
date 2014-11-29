b0ed5a2e69f983503d330674bb970d7450a87b0f
Seth Hendrick <xforever1313@aim.com>
2014-11-28 21:43:42 -0500

    Added two more points, but there is no picture with them yet.

--- js/data.js
+++ js/data.js
@@ -5,6 +5,18 @@ var mapData = {"mapData": [
      "desc" : "The greatest department ever!",
      "image" : "ce.png"
     },
+    {"title" : "Sundial",
+     "lat"   : 43.08436,
+     "lon"   : -77.66916,
+     "desc"  : "The Sundial",
+     "image" : "sundial.jpg"
+    },
+    {"title" : "The Sentinal",
+     "lat"   : 43.0846,
+     "lon"   : -77.67434,
+     "desc"  : "The Sentinal.  A sculpture made by Albert Paley.  Some say it looks like a knight riding a horse.  Others say its someone reading.  Some say its a mess.  One thing is for sure though... it is unique!",
+     "image" : "sentinal.jpg"
+    },
 ]
 };
 
