62748116bfdf5ff6c711b0115062f73d4a76d239
Seth Hendrick <seth@shendrick.net>
2014-12-03 18:58:20 -0500

    Improved the leaflet.js rendering
    Added a no image image.

--- js/data.js
+++ js/data.js
@@ -18,19 +18,19 @@ var mapData = {"mapData": [
      "lat"   : 43.08436,
      "lon"   : -77.66916,
      "desc"  : "The Sundial",
-     "image" : "sundial.jpg"
+     "image" : ""
     },
     {"title" : "The Sentinal",
      "lat"   : 43.0846,
      "lon"   : -77.67434,
      "desc"  : "The Sentinal.  A sculpture made by Albert Paley.  Some say it looks like a knight riding a horse.  Others say its someone reading.  Some say its a mess.  One thing is for sure though... it is unique!",
-     "image" : "sentinal.jpg"
+     "image" : ""
     },
 	{"title" : "Unity Sculpture",
 	 "lat"   : 43.08518,
 	 "lon"   : -77.67841,
 	 "desc"  : 'Built as a tribute to the <a href="http://en.wikipedia.org/wiki/Gracies_Dinnertime_Theatre">Gracie\'s Dinnertime Theater</a>',
-	 "image" : "gdt.jpg"
+	 "image" : ""
 	},
 ]
 };
